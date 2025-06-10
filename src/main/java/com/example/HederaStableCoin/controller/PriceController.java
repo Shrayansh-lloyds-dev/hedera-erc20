31111fdb1577226bf86ae3ca35866186c25ee939
Shrayansh-lloyds-dev <Shrayansh-Mani.Tripathi@lloydsbanking.com>
2025-06-10 17:03:33 +0530

    Adding Bruno Collection

--- src/main/java/com/example/HederaStableCoin/controller/PriceController.java
+++ src/main/java/com/example/HederaStableCoin/controller/PriceController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/prices")
+@RequestMapping("/api/getCurrencyPrices")
 public class PriceController {
 
     @Autowired
